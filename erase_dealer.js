ad08869492a0829dad76b472c48bc65c066cf123
DoTheBestMayB <xrutex@mju.ac.kr>
2021-01-10 23:40:28 +0900

    Edit popup.html

--- erase_dealer.js
+++ erase_dealer.js
@@ -1,13 +1,4 @@
 function erase_dealer_icon(){
-    let detect_element = document.querySelector('#main-area');
-
-    detect_element.addEventListener('change', function(){
-        chrome.tabs.executeScript(tabOfInterestId, {
-            frameId: 'cafe_main',
-            file: 'erase_dealer.js'
-        });
-    })
-
     let inner_iframe = document.getElementById('cafe_main').contentWindow;
     let post_table = inner_iframe.document.querySelectorAll('#main-area > div.article-board.m-tcol-c')[1];
     let ls = post_table.querySelectorAll('div.article-board.m-tcol-c > table > tbody > tr');
@@ -18,11 +9,21 @@ function erase_dealer_icon(){
     for(idx=ls.length-1; idx>=0; idx--){
         let src = ls[idx].querySelector('td.td_name > div > table > tbody > tr > td > span > img').getAttribute('src');
         
-        if(src == dealer_link){
-            ls[idx].remove();
+        if(src === dealer_link){
+            ls[idx].setAttribute('style', 'display: none;');
         }
     }
 }
 
+erase_dealer_icon()
+
+let detect_element = document.querySelector('#main-area');
+
+detect_element.addEventListener('change', function(){
+    chrome.tabs.executeScript(tabOfInterestId, {
+        frameId: 'cafe_main',
+        file: 'erase_dealer.js'
+    });
+})
 
 document.getElementById('cafe_main').addEventListener('load', erase_dealer_icon, false); 
--- handleExtension.js
+++ handleExtension.js
@@ -1,20 +1,3 @@
-// 원본 코드
-
-// let inner_iframe = document.getElementById('cafe_main').contentWindow;
-// let post_table = inner_iframe.document.querySelectorAll('#main-area > div.article-board.m-tcol-c')[1];
-// let ls = post_table.querySelectorAll('div.article-board.m-tcol-c > table > tbody > tr');
-
-// let idx;
-// let dealer_link = 'https://cafe.pstatic.net/levelicon/1/1_150.gif';
-
-// for(idx=ls.length-1; idx>=0; idx--){
-//     let src = ls[idx].querySelector('td.td_name > div > table > tbody > tr > td > span > img').getAttribute('src');
-    
-//     if(src == dealer_link){
-//         ls[idx].remove();
-//     }
-// }
-
 //차단하고 싶은 유저를 입력하고 버튼을 눌렀을 때, 차단하는 유저 반영
 document.getElementById('userAddButton').addEventListener("click", function(){
     let seler_name = document.querySelector('#seler_id').value;
