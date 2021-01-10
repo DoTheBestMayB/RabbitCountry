e2e95b8c1d25ea7c2ed18a93d996e2bc1bdbc9d4
DoTheBestMayB <xrutex@mju.ac.kr>
2021-01-10 23:42:21 +0900

    Edit popup.html

---
+++ popupScript.js
@@ -0,0 +1,20 @@
+function clickTab(evt, tabName) {
+    // Declare all variables
+    let i, tabContent, tabLinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabContent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tabLinks = document.getElementsByClassName("tabLinks");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
