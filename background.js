81864ddd38b83ee59621d77ae9ab29034248ff25
DoTheBestMayB <xrutex@mju.ac.kr>
2021-01-11 17:28:23 +0900

    checkBox 상태에 따라 dealer hide/show 확장 프로그램 아이콘 클릭하지 않아도 동작하는거 수정중

---
+++ background.js
@@ -0,0 +1,47 @@
+chrome.webNavigation.onCompleted.addListener(function(){
+    // 값을 가져오기
+    chrome.storage.sync.get(function(data){
+        let eraseDoChecked = data.eraseCheckBox;
+
+        document.querySelector('#eraseButton').checked = eraseDoChecked;
+
+        if(eraseDoChecked){
+            chrome.tabs.executeScript({
+                file: 'eraseDealer.js'
+            });
+        }
+    })
+
+    //차단하고 싶은 유저를 입력하고 버튼을 눌렀을 때, 차단하는 유저 반영
+    document.getElementById('userAddButton').addEventListener("click", function(){
+        let seler_name = document.querySelector('#seler_id').value;
+
+    });
+
+    // 업자, 등록된 유저 삭제하기
+    document.getElementById('eraseButton').addEventListener("change", function(){
+        if(this.checked){
+            // 크롬 스토리지에 상태 저장
+            chrome.storage.sync.set({
+                'eraseCheckBox': true
+            });
+
+            chrome.tabs.executeScript({
+                file: 'eraseDealer.js'
+            });
+        } else{
+            // 크롬 스토리지에 상태 저장
+            chrome.storage.sync.set({
+                'eraseCheckBox': false
+            });
+
+            chrome.tabs.executeScript({
+                file: 'recoverDealer.js'
+            });
+        }
+    });
+}, {
+    url: [
+        {urlPrefix: 'https://cafe.naver.com/joonggonara'},
+    ]
+});
--- handleExtension.js
+++ handleExtension.js
@@ -1,3 +1,16 @@
+// 값을 가져오기
+chrome.storage.sync.get(function(data){
+    let eraseDoChecked = data.eraseCheckBox;
+
+    document.querySelector('#eraseButton').checked = eraseDoChecked;
+
+    if(eraseDoChecked){
+        chrome.tabs.executeScript({
+            file: 'eraseDealer.js'
+        });
+    }
+})
+
 //차단하고 싶은 유저를 입력하고 버튼을 눌렀을 때, 차단하는 유저 반영
 document.getElementById('userAddButton').addEventListener("click", function(){
     let seler_name = document.querySelector('#seler_id').value;
@@ -5,8 +18,24 @@ document.getElementById('userAddButton').addEventListener("click", function(){
 });
 
 // 업자, 등록된 유저 삭제하기
-document.getElementById('eraseButton').addEventListener("click", function(){
-    chrome.tabs.executeScript({
-        file: 'erase_dealer.js'
-    });
+document.getElementById('eraseButton').addEventListener("change", function(){
+    if(this.checked){
+        // 크롬 스토리지에 상태 저장
+        chrome.storage.sync.set({
+            'eraseCheckBox': true
+        });
+
+        chrome.tabs.executeScript({
+            file: 'eraseDealer.js'
+        });
+    } else{
+        // 크롬 스토리지에 상태 저장
+        chrome.storage.sync.set({
+            'eraseCheckBox': false
+        });
+
+        chrome.tabs.executeScript({
+            file: 'recoverDealer.js'
+        });
+    }
 });
