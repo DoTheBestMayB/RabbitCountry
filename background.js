b43bd6e8f1609e1b4d0cda2fb2be664ff1e03f31
DoTheBestMayB <send1684@gmail.com>
2023-04-10 22:22:57 +0900

    feat: ShortCut 테스트 한 코드

--- background.js
+++ background.js
@@ -20,84 +20,84 @@ function alertPopup(msg) {
 }
 
 function addBookMark(sendResponse, is_alert=false) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        if (tabs[0] && tabs[0].url.includes('cafe.naver.com')) {
-            chrome.webNavigation.getAllFrames({tabId: tabs[0].id},
-                function(details) {
-                    let isFind = false;
-                    for (let idx=0; idx < details.length; idx ++) {
-                        if (details[idx].url.includes('cafe.naver.com/ca-fe')) {
-                            isFind = true;
-                            chrome.scripting.executeScript({
-                                    target: {tabId: tabs[0].id, frameIds: [details[idx].frameId]},
-                                    func: getUrl
-                                },
-                                (injectionResults) => {
-                                    chrome.bookmarks.getTree(function(results) {
-                                        let mainFolder = results[0].children[0];
-                                        let childFolders = results[0].children[0].children;
+    return new Promise(function (resolve, reject) {
+        chrome.tabs.query({active: true, currentWindow: true})
+            .then(function(tabs) {
+                if (tabs[0] && tabs[0].url.includes('cafe.naver.com')) {
+                    return chrome.webNavigation.getAllFrames({tabId: tabs[0].id})
+                        .then(function(details) {
+                            let isFind = false;
+                            for (let idx=0; idx < details.length; idx ++) {
+                                if (details[idx].url.includes('cafe.naver.com/ca-fe')) {
+                                    isFind = true;
+                                    chrome.scripting.executeScript({
+                                            target: {tabId: tabs[0].id, frameIds: [details[idx].frameId]},
+                                            func: getUrl
+                                        },
+                                        (injectionResults) => {
+                                            chrome.bookmarks.getTree(function(results) {
+                                                let mainFolder = results[0].children[0];
+                                                let childFolders = results[0].children[0].children;
 
-                                        for (let childIdx=0; childIdx < childFolders.length; childIdx++) {
-                                            if (childFolders[childIdx].hasOwnProperty('children') && childFolders[childIdx].title === '당근나라') {
-                                                chrome.bookmarks.create(
-                                                    {'parentId': childFolders[childIdx].id, 'title': injectionResults[0].result[0], 'url': injectionResults[0].result[1]}
-                                                );
-                                                if (sendResponse !== null) sendResponse({responseMsg: true});
-                                                if (is_alert) {
-                                                    chrome.scripting.executeScript({
-                                                        target: {tabId: tabs[0].id, frameIds: [0]},
-                                                        func: alertPopup,
-                                                        args: ["'당근나라' 폴더에 추가되었습니다!"]
-                                                    });
+                                                for (let childIdx=0; childIdx < childFolders.length; childIdx++) {
+                                                    if (childFolders[childIdx].hasOwnProperty('children') && childFolders[childIdx].title === '당근나라') {
+                                                        chrome.bookmarks.create(
+                                                            {'parentId': childFolders[childIdx].id, 'title': injectionResults[0].result[0], 'url': injectionResults[0].result[1]}
+                                                        );
+                                                        if (sendResponse !== null) sendResponse({responseMsg: true});
+                                                        if (is_alert) {
+                                                            chrome.scripting.executeScript({
+                                                                target: {tabId: tabs[0].id, frameIds: [0]},
+                                                                func: alertPopup,
+                                                                args: ["'당근나라' 폴더에 추가되었습니다!"]
+                                                            });
+                                                        }
+                                                        return true;
+                                                    }
                                                 }
-                                                return true;
-                                            }
-                                        }
 
-                                        chrome.bookmarks.create(
-                                            {'parentId': mainFolder.id, 'title': '당근나라'},
-                                            function(newFolder) {
                                                 chrome.bookmarks.create(
-                                                    {'parentId': newFolder.id, 'title': injectionResults[0].result[0], 'url': injectionResults[0].result[1]},
+                                                    {'parentId': mainFolder.id, 'title': '당근나라'},
+                                                    function(newFolder) {
+                                                        chrome.bookmarks.create(
+                                                            {'parentId': newFolder.id, 'title': injectionResults[0].result[0], 'url': injectionResults[0].result[1]},
+                                                        );
+                                                        if (sendResponse !== null) sendResponse({responseMsg: true});
+                                                        if (is_alert) {
+                                                            chrome.scripting.executeScript({
+                                                                target: {tabId: tabs[0].id, frameIds: [0]},
+                                                                func: alertPopup,
+                                                                args: ["'당근나라' 폴더에 추가되었습니다!"]
+                                                            });
+                                                        }
+                                                    },
                                                 );
-                                                if (sendResponse !== null) sendResponse({responseMsg: true});
-                                                if (is_alert) {
-                                                    chrome.scripting.executeScript({
-                                                        target: {tabId: tabs[0].id, frameIds: [0]},
-                                                        func: alertPopup,
-                                                        args: ["'당근나라' 폴더에 추가되었습니다!"]
-                                                    });
-                                                }
-                                            },
-                                        );
+                                            });
+                                        });
+                                    break;
+                                }
+                            }
+                            if (!isFind) {
+                                if (sendResponse !== null) sendResponse({responseMsg: false});
+                                if (is_alert) {
+                                    chrome.scripting.executeScript({
+                                        target: {tabId: tabs[0].id, frameIds: [0]},
+                                        func: alertPopup,
+                                        args: ["현재 카페 게시물을 보고 있지 않습니다!"]
                                     });
-                                });
-                            break;
-                        }
-                    }
-                    if (!isFind) {
-                        if (sendResponse !== null) sendResponse({responseMsg: false});
-                        if (is_alert) {
-                            chrome.scripting.executeScript({
-                                target: {tabId: tabs[0].id, frameIds: [0]},
-                                func: alertPopup,
-                                args: ["현재 카페 게시물을 보고 있지 않습니다!"]
-                            });
-                        }
-                    }
+                                }
+                                resolve(false);
+                                return false;
+                            }
+                            resolve(true);
+                            return true;
+                        });
+                } else {
+                    if (sendResponse !== null) sendResponse({responseMsg: false});
+                    resolve(false);
+                    return false;
                 }
-            )
-        } else {
-            if (sendResponse !== null) sendResponse({responseMsg: false});
-            // 네이버 카페 주소가 아닌 경우, 사용자가 잘못 눌러도 alert 하지 않는게 사용성에 좋아보여서 주석 처리
-            // if (is_alert) {
-            //     chrome.scripting.executeScript({
-            //         target: {tabId: tabs[0].id, frameIds: [0]},
-            //         func: alertPopup,
-            //         args: ["네이버 카페 게시물이 아닙니다."]
-            //     });
-            // }
-        }
+            });
     });
 }
 
@@ -174,10 +174,71 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
 
 chrome.commands.onCommand.addListener((command) => {
     if (command.toString() === 'add-bookmark') {
-        addBookMark(null, true);
+        addBookMark(null, true).then(function (result) {
+            if (result !== false) {
+                return;
+            }
+            chrome.commands.getAll((commands) => {
+                const commandKey = commands.find((cmd) => cmd.name === command.toString())?.shortcut;
+                if (!commandKey) {
+                    return;
+                }
+                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+
+                    // 1. document.dispatchEvent
+                    // chrome.scripting.executeScript({
+                    //     target: {tabId: tabs[0].id, frameIds: [0]},
+                    //     func: handleShortCut,
+                    //     args: [commandKey],
+                    // });
+
+                    // 2. debugger
+                    chrome.debugger.attach({ tabId: tabs[0].id }, "1.0").then(function() {
+                        chrome.debugger.sendCommand({tabId: tabs[0].id}, 'Input.dispatchKeyEvent', {
+                            type: 'rawKeyDown',
+                            windowsVirtualKeyCode: 0x11,
+                            nativeVirtualKeyCode: 0x11,
+                        }).then(function() {
+                            chrome.debugger.sendCommand({tabId: tabs[0].id}, 'Input.dispatchKeyEvent', {
+                                type: 'rawKeyDown',
+                                windowsVirtualKeyCode: 0x42,
+                                nativeVirtualKeyCode: 0x42,
+                            }).then(function() {
+                                chrome.debugger.detach({ tabId: tabs[0].id });
+                            });
+                        });
+                    })
+                });
+            });
+        });
     }
 });
 
+function handleShortCut(shortCut) {
+    function parseShortcut(shortcutString) {
+        const parts = shortcutString.split('+');
+        return {
+            key: parts.pop().toUpperCase(),
+            ctrlKey: parts.includes('Ctrl'),
+            shiftKey: parts.includes('Shift'),
+            altKey: parts.includes('Alt'),
+            metaKey: parts.includes('Cmd') || parts.includes('Meta'),
+        };
+    }
+
+    const { key, ctrlKey, shiftKey, altKey, metaKey } = parseShortcut(shortCut);
+    console.log(key, ctrlKey, shiftKey, altKey, metaKey);
+
+    document.dispatchEvent(
+        new KeyboardEvent('keydown', {
+            key,
+            ctrlKey,
+            shiftKey,
+            altKey,
+            metaKey,
+        })
+    );
+}
 
 /*
 처음 설치시 확인할 것들
